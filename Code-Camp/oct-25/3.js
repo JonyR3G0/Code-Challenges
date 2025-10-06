0d25c7c351e1fb44179e4c9b7db1ea9a7b2ef6fe
J0n4s4n <jonh.reyes.go@gmail.com>
2025-10-06 03:26:58 -0600

    feat: :white_check_mark: Passed test 03-10-2025

--- Code-Camp/oct-25/3.js
+++ Code-Camp/oct-25/3.js
@@ -7,17 +7,41 @@
  * It contains at least one number.
  * It contains at least one special character
  * from this set: !, @, #, $, %, ^, &, or *.
- * @param {*} password 
- * @returns 
+ * @param {*} password
+ * @returns
  */
 function checkStrength(password) {
-    let validations = 0
-    const pswA = Array.from(password)
-    pswA.length >= 8 ? validations + 1 : null
-    
+  const symbols = /[!@#$%^&*]/;
+  const numbers = /\d/;
+  const upper = /[A-Z]/;
+  const lower = /[a-z]/;
+  let validations = 0;
+  const pswA = Array.from(password);
 
-   if(!(validations > 2 && validations < 4 ? 'medium' : 'strong')){
-    return 'weak'
-   }
-  return 'Error';
+  pswA.length >= 8 ? (validations += 1) : null;
+  symbols.test(password) ? (validations += 1) : null;
+  numbers.test(password) ? (validations += 1) : null;
+  upper.test(password) && lower.test(password) ? (validations += 1) : null;
+
+  let answ = validations >= 2 && validations < 4 ? "medium" : "strong";
+  validations < 2 ? (answ = "weak") : null;
+
+  return answ;
 }
+
+/** 
+ *  It's 3:25 am fr if isn't the most optimized answer, at least it works just fine.
+ *  console.log(checkStrength("123456"));
+ * Tests
+Passed:1. checkStrength("123456") should return "weak".
+Passed:2. checkStrength("pass!!!") should return "weak".
+Passed:3. checkStrength("Qwerty") should return "weak".
+Passed:4. checkStrength("PASSWORD") should return "weak".
+Passed:5. checkStrength("PASSWORD!") should return "medium".
+Passed:6. checkStrength("PassWord%^!") should return "medium".
+Passed:7. checkStrength("qwerty12345") should return "medium".
+Passed:8. checkStrength("PASSWORD!") should return "medium".
+Passed:9. checkStrength("PASSWORD!") should return "medium".
+Passed:10. checkStrength("S3cur3P@ssw0rd") should return "strong".
+Passed:11. checkStrength("C0d3&Fun!") should return "strong".
+*/
