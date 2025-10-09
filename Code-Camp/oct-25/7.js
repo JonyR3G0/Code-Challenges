9a820d839ddfdcb5842ca8b17b32daa3d3a5f24c
J0n4s4n <jonh.reyes.go@gmail.com>
2025-10-08 18:26:32 -0600

    feat: :white_check_mark: Passed FCC challenge 7-oct-2025

--- Code-Camp/oct-25/7.js
+++ Code-Camp/oct-25/7.js
@@ -1,6 +1,5 @@
-
 /**
- * You are given a matrix of numbers (an array of arrays), representing potential landing spots for your rover. Find 
+ * You are given a matrix of numbers (an array of arrays), representing potential landing spots for your rover. Find
  * the safest landing spot based on the following rules:
  * Each spot in the matrix will contain a number from 0-9, inclusive.
  * Any 0 represents a potential landing spot.
@@ -10,35 +9,52 @@
  * Ignore out-of-bounds neighbors (corners and edges just have fewer neighbors).
  * Return the indices of the safest landing spot. There will always only be one safest spot.
  * @author Jonathan Reyes
- * @param {*} matrix 
- * @returns {*} 
+ * @param {*} matrix
+ * @returns {*}
  */
 function findLandingSpot(matrix) {
-    const landingCoordinates = []
-// [9, 0, 3]
-// [7, 0, 4]
-// [8, 0, 5]
-let sumaRowCero = null
-let coordenadas = null
-    for (let n = 0; n < matrix.length; n++) {
-        for (let i = 0; i < matrix[n].length; i++) {
-            const actualRow = matrix[n]
-            if(actualRow[i] === 0){
-                coordenadas = [[i], [n]]
-                sumaRowCero = (i <= 0 ? 0 : actualRow[i - 1]) + (i >= actualRow.length - 1 ? 0 : actualRow[i + 1])
-                console.log(`0D resum | ${sumaRowCero} en mat. ${n} en pos ${i}`)
+  let landingCoordinates = [];
+  let lowestAccumulative = null;
+  for (let n = 0; n < matrix.length; n++) {
+    // Every col
+    for (let i = 0; i < matrix[n].length; i++) {
+      const actualRow = matrix[n];
+      if (actualRow[i] === 0) {
+        const dangerAbove = n !== 0 ? matrix[n - 1][i] : 0;
+        const dangerBelow = n !== matrix.length - 1 ? matrix[n + 1][i] : 0;
 
-            }
+        const dangerAhead = actualRow[i + 1] ?? 0;
+        const dangerBehind = actualRow[i - 1] ?? 0;
+
+        const dangerX = dangerAbove + dangerBelow;
+        const dangerY = dangerAhead + dangerBehind;
+
+        const dangerAcumulative = dangerX + dangerY;
+
+        console.log(dangerAcumulative);
+        if (
+          dangerAcumulative < lowestAccumulative ||
+          lowestAccumulative === null
+        ) {
+          lowestAccumulative = dangerAcumulative;
+          landingCoordinates = [n, i];
         }
+      }
     }
+  }
   return landingCoordinates;
 }
-// [1, 2, 1], 
-// [0, 0, 2], 
-// [3, 0, 0]
-findLandingSpot([[1, 2, 1], [0, 0, 2], [3, 0, 0]])
+
+// console.log(
+//   findLandingSpot([
+//     [1, 0],
+//     [2, 0],
+//   ])
+// );
 /**
- * 1. findLandingSpot([[1, 0], [2, 0]]) should return [0, 1].
-Waiting:2. findLandingSpot([[9, 0, 3], [7, 0, 4], [8, 0, 5]]) should return [1, 1].
-Waiting:3. findLandingSpot([[1, 2, 1], [0, 0, 2], [3, 0, 0]]) should return [2, 2].
+ * 1Tests
+Passed:1. findLandingSpot([[1, 0], [2, 0]]) should return [0, 1].
+Passed:2. findLandingSpot([[9, 0, 3], [7, 0, 4], [8, 0, 5]]) should return [1, 1].
+Passed:3. findLandingSpot([[1, 2, 1], [0, 0, 2], [3, 0, 0]]) should return [2, 2].
+Passed:4. findLandingSpot([[9, 6, 0, 8], [7, 1, 1, 0], [3, 0, 3, 9], [8, 6, 0, 9]]) should return [2, 1].
  */
