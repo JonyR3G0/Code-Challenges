46012c57b0f83dc79e50c5ce4def705c036f9e13
J0n4s4n <jonh.reyes.go@gmail.com>
2025-10-11 16:37:34 -0600

    refactor: Clean up code by removing unused variables and comments

--- LeetCode/383. Ransom Note.js
+++ LeetCode/383. Ransom Note.js
@@ -11,29 +11,37 @@
 var canConstruct = function (ransomNote, magazine) {
     let hashMaga = []
     let hashRansom = []
-    let status = []
+
     for (let n = 0; n < magazine.length; n++) {
+
         const answ = hashMaga.findIndex((element) => element.letter === magazine[n])
+
         if (answ === -1) {
             hashMaga.push({ letter: magazine[n], quant: 1 })
             continue
         }
+
         hashMaga[answ].quant = hashMaga[answ].quant + 1
+
     }
+
     for (let n = 0; n < ransomNote.length; n++) {
+
         const answ = hashRansom.findIndex((element) => element.letter === ransomNote[n])
 
         if (answ === -1) {
             hashRansom.push({ letter: ransomNote[n], quant: 1 })
             continue
         }
+
         hashRansom[answ].quant = hashRansom[answ].quant + 1
     }
-    console.log(hashMaga, hashRansom)
+
     for (let n = 0; n < hashRansom.length; n++) {
+        
         const answ = hashMaga.findIndex((element) => element.letter === hashRansom[n].letter)
+
         if (answ === -1 || hashMaga[answ].quant >= hashRansom[n].quant === false) return false
-        // console.log(`Buscando letra ${hashMaga[answ].letter}`)
     }
     return true
 };
