36690162b07c95ef34cfcb3e8925b76ade6170f8
Jonathan ReGo <jonh.reyes.go@gmail.com>
2026-01-04 20:31:01 -0600

    feat: :construction: Pseudocode idea
    This is my first idea for aproaching this challenge. It is iterative (i think?) its pseudocode, so, tomorrow i'm gonna try to work it out

--- LeetCode/206. Reverse Linked List.js
+++ LeetCode/206. Reverse Linked List.js
@@ -4,11 +4,27 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
+ * 
+ * EXAMPLE=>
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
  */
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
+    //Solucion iterativa?
+    const headVal = []
+    const reversed = {}
+    //Iterar hasta que no haya un nodo siguiente
+    while (this.next === undefined) {
+        //Leer valor de nodo y guardarlo en el array headVal
+    }
+    for (let i = 0; i < headVal.length; i++) {
+        const element = array[i];
+        //Crear linked list a partir del head val
+    }
+    return reversed
 };
